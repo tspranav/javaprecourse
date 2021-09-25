197c43b73a97ed3c2dbe17940041279c58f7f669
Sai Pranav <tsp.workacc@gmail.com>
2021-09-25 20:46:09 +0530

    first commit

---
+++ Compare.java
@@ -0,0 +1,18 @@
+public class Compare{
+	public static void main(String[] args){
+		int a= 5;
+		int b=14;
+		int c=17;
+		
+		if(a>b && a>c){
+			System.out.print(a+" is the greatest number");
+		}
+		else if(b>a && b>c){
+			System.out.print(b+" is the greatest number");
+		}
+		else{
+			System.out.print(c+" is the greatest number");
+		}
+	}
+}
+			
